8998319c1f6c3a76be87a132d485c1fda312828a
rkahsu <142762205+rkahsu@users.noreply.github.com>
2023-08-20 18:45:57 -0700

    Add files via upload

---
+++ Module1.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+ 
+class main_class {
+    public static void main(String[] args){
+//declare variables
+    
+      String FirstName = "Zina";
+      String LastName = "Clover";
+      String StreetAddress = "1234 Fillmore Street";
+      String City = "San Francisco";
+      String Zipcode = "94115";
+// print persons information
+ 
+      System.out.println("First name: " + FirstName);
+      System.out.println("Last name: " + LastName);
+      System.out.println("Street address: " + StreetAddress);
+      System.out.println("City: " + City);
+      System.out.println("Zip Code: " + Zipcode);
+    }
+}
